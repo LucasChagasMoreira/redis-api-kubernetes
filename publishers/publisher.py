bb788dfdab663fcd3be5990b2c3d7d34f567ce13
LucasChagasMoreira <97477970+LucasChagasMoreira@users.noreply.github.com>
2024-07-31 20:31:05 -0300

    model..yml

--- publishers/publisher.py
+++ publishers/publisher.py
@@ -4,21 +4,21 @@ import json
 import base64
 import os
 
-redis_client = redis.Redis(host="35.239.233.36",port=6379)
+redis_client = redis.Redis(host="34.42.25.221",port=6379)
 channel = "MNIST IMAGES"
 
 user = ""
 
 image_path = "inputs"
-images_names_list = os.listdir(image_path)
-print(images_names_list)
 
-
-for image_input in images_names_list:
-    image = cv2.imread(f"{image_path}/{image_input}")
+image_name = input("digite um numero desejado: ")
+while image_name != "exit":
+    image = cv2.imread(f"{image_path}/{image_name}")
     image = cv2.imencode('.jpg', image)[1].tobytes()
     image = base64.b64encode(image).decode('utf-8')
 
-    message = json.dumps({"image_name": image_input, "image": image, "user": user, "Prediction": None})
+    message = json.dumps({"image_name": image_name, "image": image, "user": user, "Prediction": None})
     redis_client.publish(channel, message)
+    
+    image_name = input("digite um numero desejado: ")
     
